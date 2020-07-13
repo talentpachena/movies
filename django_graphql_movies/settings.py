fd159f848d3d5cc9b55d2a5e4fef48d42aaa01b1
talentpachena <45259251+talentpachena@users.noreply.github.com>
2020-07-13 13:43:57 +0200

    Update settings.py

--- django_graphql_movies/settings.py
+++ django_graphql_movies/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x0&h=ru#=i0!t3kp_@49*&8%usx!r4gt8wcrsgvajtir11@y-r'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
